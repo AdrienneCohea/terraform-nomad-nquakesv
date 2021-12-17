ae9046a93e7e61c93bf7be75749f170ba4c086a4
Adrienne Cohea <adriennecohea@gmail.com>
2021-12-17 14:54:55 -0800

    Add descriptions

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "datacenter" {
-  type    = string
-  default = "dc1"
+  type        = string
+  default     = "dc1"
+  description = "The Nomad datacenter in which to run Quake"
 }
 
 variable "rcon_password" {
-  type    = string
+  type        = string
+  description = "The password for RCON (remote console)"
 }
